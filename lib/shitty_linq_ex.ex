f64788d6862fb0308d33fea40e8d75a107825a4e
Patrick Tone <patrick@madronesoftware.com>
2020-09-30 14:46:20 +0200

    implements where with the indexed predicate

--- lib/shitty_linq_ex.ex
+++ lib/shitty_linq_ex.ex
@@ -14,12 +14,24 @@ defmodule ShittyLinqEx do
   ## Examples
 
     iex> import ShittyLinqEx, only: [where: 2]
-    iex> where([1, 2, 3], fn x -> rem(x, 2) == 0 end)
-    [2]
+    iex> where(
+    ...>  ["apple", "passionfruit", "banana", "mango", "orange", "blueberry", "grape", "strawberry"],
+    ...>  fn fruit -> String.length(fruit) < 6 end)
+    ["apple", "mango", "grape"]
+
+    iex> import ShittyLinqEx, only: [where: 2]
+    iex> where(
+    ...>  [0, 30, 20, 15, 90, 85, 40, 75],
+    ...>  fn number, index -> number <= index * 10 end)
+    [0, 20, 15, 40]
 
   """
-  def where(source, predicate) when is_list(source) do
+  def where(source, predicate) when is_list(source) and is_function(predicate, 1) do
     where_list(source, predicate)
+  end
+
+  def where(source, predicate) when is_list(source) and is_function(predicate, 2) do
+    where_list(source, predicate, 0)
   end
 
   defp where_list([head | tail], fun) do
@@ -30,6 +42,17 @@ defmodule ShittyLinqEx do
   end
 
   defp where_list([], _fun) do
+    []
+  end
+
+  defp where_list([head | tail], fun, index) do
+    case fun.(head, index) do
+      true -> [head | where_list(tail, fun, index + 1)]
+      _ -> where_list(tail, fun, index + 1)
+    end
+  end
+
+  defp where_list([], _fun, _index) do
     []
   end
 end
