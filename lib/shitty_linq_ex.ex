bb2245ad3d4decec973ca77bd4f9c73a12f1e1c6
Patrick Tone <patrick@madronesoftware.com>
2020-10-02 12:09:07 +0200

    adds aggregate methods

--- lib/shitty_linq_ex.ex
+++ lib/shitty_linq_ex.ex
@@ -4,12 +4,189 @@ defmodule ShittyLinqEx do
   """
 
   @doc """
+  Applies an accumulator function over a sequence. The specified seed value is used as the initial
+  accumulator value, and the specified function is used to select the result value.
+
+  ## Parameters
+
+  - `source`: an enumerable to aggregate over.
+  - `seed`: the initial accumulator value.
+  - `func`: an accumulator function to be invoked on each element.
+  - `resultSelector`: a function to transform the final accumulator value into the result value.
+
+  ## Returns
+
+  The transformed final accumulator value.
+
+  ## Examples
+
+    iex> import ShittyLinqEx, only: [aggregate: 4]
+    iex> fruits = ["apple", "mango", "orange", "passionfruit", "grape"]
+    iex> aggregate(
+    ...>  fruits,
+    ...>  "banana",
+    ...>  fn next, longest ->
+    ...>    if String.length(next) > String.length(longest) do
+    ...>      next
+    ...>    else
+    ...>      longest
+    ...>    end
+    ...>  end,
+    ...>  &String.upcase/1)
+    "PASSIONFRUIT"
+
+  """
+  def aggregate(source, seed, func, resultSelector)
+      when is_list(source) and is_function(func, 2) and is_function(resultSelector, 1) do
+    :lists.foldl(func, seed, source)
+    |> resultSelector.()
+  end
+
+  def aggregate(first..last, seed, func, resultSelector)
+      when is_function(func, 2) and is_function(resultSelector, 1) do
+    if first <= last do
+      aggregate_range_inc(first, last, seed, func)
+    else
+      aggregate_range_dec(first, last, seed, func)
+    end
+    |> resultSelector.()
+  end
+
+  def aggregate(%{} = source, seed, func, resultSelector)
+      when is_function(func, 2) and is_function(resultSelector, 1) do
+    :maps.fold(
+      fn key, value, accumulator -> func.({key, value}, accumulator) end,
+      seed,
+      source
+    )
+    |> resultSelector.()
+  end
+
+  def aggregate(source, seed, func, resultSelector)
+      when is_list(source) and is_function(func, 2) and is_function(resultSelector, 1) do
+    :lists.foldl(func, seed, source)
+    |> resultSelector.()
+  end
+
+  def aggregate(first..last, seed, func, resultSelector)
+      when is_function(func, 2) and is_function(resultSelector, 1) do
+    if first <= last do
+      aggregate_range_inc(first, last, seed, func)
+    else
+      aggregate_range_dec(first, last, seed, func)
+    end
+    |> resultSelector.()
+  end
+
+  @doc """
+  Applies an accumulator function over a sequence.
+  The specified seed value is used as the initial accumulator value.
+
+  ## Parameters
+
+  - `source`: an enumerable to aggregate over.
+  - `seed`: the initial accumulator value.
+  - `func`: an accumulator function to be invoked on each element.
+
+  ## Returns
+
+  The final accumulator value.
+
+  ## Examples
+
+    iex> import ShittyLinqEx, only: [aggregate: 3]
+    iex> ints = [4, 8, 8, 3, 9, 0, 7, 8, 2]
+    iex> aggregate(
+    ...>  ints,
+    ...>  0,
+    ...>  fn next, total ->
+    ...>    if rem(next, 2) == 0 do
+    ...>      total + 1
+    ...>    else
+    ...>      total
+    ...>    end
+    ...>  end)
+    6
+
+    iex> import ShittyLinqEx, only: [aggregate: 3]
+    iex> aggregate(4..1, 1, &*/2)
+    24
+
+    iex> import ShittyLinqEx, only: [aggregate: 3]
+    iex> aggregate(1..3, 1, &+/2)
+    7
+
+    iex> import ShittyLinqEx, only: [aggregate: 3]
+    iex> letters_to_numbers = %{a: 1, b: 2, c: 3}
+    iex> aggregate(
+    ...>  letters_to_numbers,
+    ...>  [],
+    ...>  fn {key, _value}, keys -> [key | keys] end)
+    [:c, :b, :a]
+
+  """
+  def aggregate(source, seed, func)
+      when is_list(source) and is_function(func, 2) do
+    :lists.foldl(func, seed, source)
+  end
+
+  def aggregate(first..last, seed, func)
+      when is_function(func, 2) do
+    if first <= last do
+      aggregate_range_inc(first, last, seed, func)
+    else
+      aggregate_range_dec(first, last, seed, func)
+    end
+  end
+
+  def aggregate(%{} = source, seed, func)
+      when is_function(func, 2) do
+    :maps.fold(
+      fn key, value, acc -> func.({key, value}, acc) end,
+      seed,
+      source
+    )
+  end
+
+  @doc """
+  Applies an accumulator function over a sequence.
+
+  ## Parameters
+
+  - `source`: an enumerable to aggregate over.
+  - `func`: an accumulator function to be invoked on each element.
+
+  ## Returns
+
+  The final accumulator value.
+
+  ## Examples
+
+  iex> import ShittyLinqEx, only: [aggregate: 2]
+  iex> sentence = "the quick brown fox jumps over the lazy dog"
+  iex> words = String.split(sentence)
+  iex> aggregate(
+  ...>  words,
+  ...>  fn word, workingSentence -> word <> " " <> workingSentence end)
+  "dog lazy the over jumps fox brown quick the"
+
+  """
+  def aggregate([head | tail], func)
+      when is_function(func, 2) do
+    aggregate(tail, head, func)
+  end
+
+  @doc """
   Filters a sequence of values based on a predicate.
 
-  Where `source` is an enumerable to filter.
-  Where `predicate` is a function to test each element for a condition.
+  ## Parameters
 
-  Returns an enumerable that contains elements from the input sequence that satisfy the condition.
+  - `source`: an enumerable to filter.
+  - `predicate`: a function to test each element for a condition.
+
+  ## Returns
+
+  An enumerable that contains elements from the input sequence that satisfy the condition.
 
   ## Examples
 
@@ -32,6 +209,22 @@ defmodule ShittyLinqEx do
 
   def where(source, predicate) when is_list(source) and is_function(predicate, 2) do
     where_list(source, predicate, 0)
+  end
+
+  defp aggregate_range_inc(first, first, seed, func) do
+    func.(first, seed)
+  end
+
+  defp aggregate_range_inc(first, last, seed, func) do
+    aggregate_range_inc(first + 1, last, func.(first, seed), func)
+  end
+
+  defp aggregate_range_dec(first, first, seed, func) do
+    func.(first, seed)
+  end
+
+  defp aggregate_range_dec(first, last, seed, func) do
+    aggregate_range_dec(first - 1, last, func.(first, seed), func)
   end
 
   defp where_list([head | tail], fun) do
