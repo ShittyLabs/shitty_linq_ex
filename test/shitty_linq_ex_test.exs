70c08674ba92bc01c78743f8e29f4aeefbd1b457
volcov <volcov.bruno@gmail.com>
2020-10-02 13:23:06 -0300

    add tests for reverse

--- test/shitty_linq_ex_test.exs
+++ test/shitty_linq_ex_test.exs
@@ -1,4 +1,18 @@
 defmodule ShittyLinqExTest do
   use ExUnit.Case, async: true
   doctest ShittyLinqEx
+
+  alias ShittyLinqEx
+
+  test "reverse of empty list" do
+    assert ShittyLinqEx.reverse([]) == []
+  end
+
+  test "reverse of normal list" do
+    assert ShittyLinqEx.reverse([1, 3, 5, 7]) == [7, 5, 3, 1]
+  end
+
+  test "reverse of big list" do
+    assert ShittyLinqEx.reverse(Enum.to_list(1..1_000_000)) == Enum.to_list(1_000_000..1)
+  end
 end
